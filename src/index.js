4a13c075495f683644a94caa892080921d2f6164
chernyshevdi <cllmegod@gmail.com>
2021-03-15 09:43:50 +0300

    finally

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 module.exports = function reverse (n) {
-    let q = String(num).split('').reverse()//.join('');
+    let q = String(n).split('').reverse();
     let w = q.filter(function(number) {
-      return number > 0
+      return number >= 0
     })
     let e = w.join('')
     return Number(e)
