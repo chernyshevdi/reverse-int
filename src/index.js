fd98ee635d51229ac25eae0d39772e80ebf8ec1c
chernyshevdi <cllmegod@gmail.com>
2021-03-15 09:41:06 +0300

    change function

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 module.exports = function reverse (n) {
-    let q = String(num).split('').reverse().join(',')
-    return Number(q);
+    let q = String(num).split('').reverse()//.join('');
+    let w = q.filter(function(number) {
+      return number > 0
+    })
+    let e = w.join('')
+    return Number(e)
 }
